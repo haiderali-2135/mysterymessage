f1731df79fd51893e4ebfade23574c83e7cb799c
haiderali-2135 <haiderali10986@gmail.com>
2025-01-16 19:25:03 +0500

    created Navbar and Message Card

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -60,14 +60,14 @@ const page = () =>{
   return(
 
     <>
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+    <div className="flex justify-center items-center min-h-screen bg-gray-700">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
             Join Mystery Message
           </h1>
           <p className="mb-4">Sign in to start your anonymous adventure</p>
-
+        </div>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
 
@@ -111,10 +111,16 @@ const page = () =>{
 
             </form>
           </Form>
-    
+          <div className="text-center mt-4">
+          <p>
+            Not a member yet?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
         </div>
       </div>
-    </div>
     </>
 
   )
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -87,14 +87,14 @@ const page = () =>{
   return(
 
     <>
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+    <div className="flex justify-center items-center min-h-screen bg-gray-700">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
             Join Mystery Message
           </h1>
           <p className="mb-4">Sign up to start your anonymous adventure</p>
-
+        </div>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
               <FormField 
@@ -171,7 +171,6 @@ const page = () =>{
     
         </div>
       </div>
-    </div>
     </>
 
   )
