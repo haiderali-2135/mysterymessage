f24779e45cf34ac792ac0eba0f9fa3f080b94f5e
haiderali-2135 <haiderali10986@gmail.com>
2025-01-17 22:17:00 +0500

    created user dashboard and message page

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -4,6 +4,6 @@ import { Message } from "@/model/User";
 export interface ApiResponse{
     success: Boolean;
     message: String;
-    isAcceptingMessages ?: Boolean;
+    isAcceptingMessage ?: Boolean;
     messages?:Array<Message>
 }
